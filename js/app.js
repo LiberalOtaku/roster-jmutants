bd7a3cf522542f6fe5241fc80d9c63096cbe8857
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-01 15:28:50 -0400

    Reorganized dl as DOM element, wrapped in IIFE.

--- js/app.js
+++ js/app.js
@@ -1,42 +1,44 @@
-$(document).foundation()
+$(document).foundation();
 
-var buildColorDiv = function(color) {
-  return '<div style="height: 20px; width: 100px; background-color: ' + color + '"></div>';
-};
-
-var buildList = function(list) {
-  var dl = document.createElement('dl');
-  dl.style.border = '1px solid red';
-  dl.innerHTML += buildListItem('Name', list.firstName) +
-    buildListItem('Hair Color', list.hairColor) +
-    buildListItem('Age', list.age) +
-    buildListItem('Birthplace', list.birthplace);
-
-  return dl;
-};
-
-var buildListItem = function(term, definition) {
-  return ' \
-    <li> \
-      <dt>' + term + '</dt> \
-      <dd>' + definition + '</dd> \
-    </li>';
-};
-
-var addDetails = function(ev) {
-  ev.preventDefault();
-  var details = document.querySelector('div.details');
-  var hairColor = this.hairColor.value;
-
-  var list = {
-    firstName: this.firstName.value,
-    hairColor: buildColorDiv(hairColor),
-    age: this.age.value,
-    birthplace: this.birthplace.value
+(function() {
+  var buildColorDiv = function(color) {
+    return '<div style="height: 20px; width: 100px; background-color: ' + color + '"></div>';
   };
 
-  details.appendChild(buildList(list));
-};
+  var buildList = function(list) {
+    var dl = document.createElement('dl');
+    dl.style.border = '1px solid red';
+    dl.innerHTML += buildListItem('Name', list.firstName) +
+      buildListItem('Hair Color', list.hairColor) +
+      buildListItem('Age', list.age) +
+      buildListItem('Birthplace', list.birthplace);
 
-var myForm = document.querySelector('form');
-myForm.onsubmit = addDetails;
+    return dl;
+  };
+
+  var buildListItem = function(term, definition) {
+    return ' \
+      <li> \
+        <dt>' + term + '</dt> \
+        <dd>' + definition + '</dd> \
+      </li>';
+  };
+
+  var addDetails = function(event) {
+    event.preventDefault();
+    var details = document.querySelector('div.details');
+    var hairColor = this.hairColor.value;
+
+    var list = {
+      firstName: this.firstName.value,
+      hairColor: buildColorDiv(hairColor),
+      age: this.age.value,
+      birthplace: this.birthplace.value
+    };
+
+    details.appendChild(buildList(list));
+  };
+
+  var myForm = document.querySelector('form');
+  myForm.onsubmit = addDetails;
+})();
