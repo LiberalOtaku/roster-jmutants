58e7570a8ef5c734adcaca97cfca314ca774a4b0
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-01 17:24:10 -0400

    Created roster w/ mega bonus credit.

--- js/app.js
+++ js/app.js
@@ -1,49 +1,62 @@
 $(document).foundation();
 
+var i = 0;
+
 var app = {
 
   init: function() {
     var myForm = document.querySelector('form');
-    myForm.onsubmit = this.addDetails;
+    var myDelete = document.querySelectorAll('a.delete');
+    var myPromote = document.querySelectorAll('a.promote');
+    myForm.onsubmit = this.addName;
+    // myDelete.onclick = this.deleteName;
+    // myPromote.onclick = this.promoteName;
   },
 
-  buildColorDiv: function(color) {
-    return '<div style="height: 20px; width: 100px; background-color: ' + color + '"></div>';
-  },
-
-  buildList: function(list) {
+  buildList: function(name) {
     var dl = document.createElement('dl');
-    dl.style.border = '1px solid red';
-    dl.innerHTML += this.buildListItem('Name', list.firstName) +
-      this.buildListItem('Hair Color', list.hairColor) +
-      this.buildListItem('Age', list.age) +
-      this.buildListItem('Birthplace', list.birthplace);
+    dl.id = i;
+    i++;
+    dl.style.border = '0px solid blue';
+    dl.innerHTML += this.buildListItem(name);
 
     return dl;
   },
 
-  buildListItem: function(term, definition) {
+  buildListItem: function(term) {
     return ' \
       <li> \
         <dt>' + term + '</dt> \
-        <dd>' + definition + '</dd> \
+        <dd> \
+          <a href="#" class="delete"><u>delete</u></a> \
+          <a href="#" class="promote"><u>promote</u></a> \
+        </dd> \
       </li>';
   },
 
-  addDetails: function(event) {
+  addName: function(event) {
     event.preventDefault();
-    var details = document.querySelector('div.details');
-    var hairColor = this.hairColor.value;
+    var roster = document.querySelector('div.roster');
+    var firstName = this.firstName.value;
 
-    var list = {
-      firstName: this.firstName.value,
-      hairColor: app.buildColorDiv(hairColor),
-      age: this.age.value,
-      birthplace: this.birthplace.value
-    };
+    roster.insertBefore(app.buildList(firstName), roster.firstChild);
+  },
 
-    details.appendChild(app.buildList(list));
-  }
+  // deleteName: function(event) {
+  //   event.preventDefault();
+  //
+  // },
+  //
+  // promoteName: function(event) {
+  //   event.preventDefault();
+  //   var dl = document.getElementById(this.id);
+  //   if (dl.style.border = '0px solid blue') {
+  //     dl.style.border = '1px solid blue';
+  //   }
+  //   else {
+  //     dl.style.border = '0px solid blue';
+  //   }
+  // }
 };
 
 app.init();
