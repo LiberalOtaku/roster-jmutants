e4e814ca92a6c739d080319ab67d48d4f484fd58
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-01 15:39:38 -0400

    Refactored as object with function properties.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,49 @@
 $(document).foundation();
 
-(function() {
-  var buildColorDiv = function(color) {
-    return '<div style="height: 20px; width: 100px; background-color: ' + color + '"></div>';
-  };
+var app = {
 
-  var buildList = function(list) {
+  init: function() {
+    var myForm = document.querySelector('form');
+    myForm.onsubmit = this.addDetails;
+  },
+
+  buildColorDiv: function(color) {
+    return '<div style="height: 20px; width: 100px; background-color: ' + color + '"></div>';
+  },
+
+  buildList: function(list) {
     var dl = document.createElement('dl');
     dl.style.border = '1px solid red';
-    dl.innerHTML += buildListItem('Name', list.firstName) +
-      buildListItem('Hair Color', list.hairColor) +
-      buildListItem('Age', list.age) +
-      buildListItem('Birthplace', list.birthplace);
+    dl.innerHTML += this.buildListItem('Name', list.firstName) +
+      this.buildListItem('Hair Color', list.hairColor) +
+      this.buildListItem('Age', list.age) +
+      this.buildListItem('Birthplace', list.birthplace);
 
     return dl;
-  };
+  },
 
-  var buildListItem = function(term, definition) {
+  buildListItem: function(term, definition) {
     return ' \
       <li> \
         <dt>' + term + '</dt> \
         <dd>' + definition + '</dd> \
       </li>';
-  };
+  },
 
-  var addDetails = function(event) {
+  addDetails: function(event) {
     event.preventDefault();
     var details = document.querySelector('div.details');
     var hairColor = this.hairColor.value;
 
     var list = {
       firstName: this.firstName.value,
-      hairColor: buildColorDiv(hairColor),
+      hairColor: app.buildColorDiv(hairColor),
       age: this.age.value,
       birthplace: this.birthplace.value
     };
 
-    details.appendChild(buildList(list));
-  };
+    details.appendChild(app.buildList(list));
+  }
+};
 
-  var myForm = document.querySelector('form');
-  myForm.onsubmit = addDetails;
-})();
+app.init();
