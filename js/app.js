38d37a8dcae68655d800f9a19c6089d4b81a68c9
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-01 13:50:55 -0400

    Moved script to app.js, added app.css, other cleanup.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 $(document).foundation()
+
+var myForm = document.querySelector('form');
+myForm.onsubmit = function(ev) {
+  ev.preventDefault();
+  var details = document.querySelector('div.details');
+
+  var firstName = this.firstName.value;
+  var hairColor = this.hairColor.value;
+  var age = this.age.value;
+  var birthplace = this.birthplace.value;
+
+  var colorDiv = '<div style="height: 20px; width: 100px; background-color: ' + hairColor + '"></div>';
+
+  details.innerHTML += ' \
+    <dl> \
+      <dt>Name</dt> \
+      <dd>' + firstName + '</dd> \
+        \
+      <dt>Hair Color</dt> \
+      <dd>' + colorDiv + '</dd> \
+        \
+      <dt>Age</dt> \
+      <dd>' + age + '</dd> \
+        \
+      <dt>Birthplace</dt> \
+      <dd>' + birthplace + '</dd> \
+    </dl>';
+};
