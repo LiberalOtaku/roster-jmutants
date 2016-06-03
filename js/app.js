77ddd62ef6037303e796962bb88fac045b4854c2
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-03 16:06:51 -0400

    Updated buttons with icons and fixed sizing.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ var app = {
   // initialize app
   init: function(listSelector) {
     this.list = document.querySelector(listSelector);
+    this.roster = [];
     this.setupEventListeners();
+    this.list.innerHTML = localStorage.getItem('roster');
   },
 
   setupEventListeners: function() {
@@ -18,7 +20,7 @@ var app = {
 
     var li = document.createElement('li');
     var dt = document.createElement('dt');
-    dt.innerText += name;
+    dt.innerText = name;
     li.appendChild(dt);
 
     var dd = document.createElement('dd');
@@ -34,7 +36,7 @@ var app = {
 
     // create edit button
     var editLink = this.buildLink({
-      text: 'edit',
+      contents: '<i class="fa fa-pencil fa-lg"></i>',
       class: "edit button small radius secondary",
       handler: function() {
         dt.innerText = '';
@@ -46,7 +48,7 @@ var app = {
 
     // create update button
     var updateLink = this.buildLink({
-      text: 'update',
+      contents: '<i class="fa fa-check fa-lg"></i>',
       class: "update button small radius success",
       handler: function() {
         if (input.value !== '') {
@@ -54,24 +56,26 @@ var app = {
           dt.innerText = input.value;
 
           ul.replaceChild(editLink, updateLink);
+          app.saveList();
         }
       }
     });
 
     // create delete button
     var deleteLink = this.buildLink({
-      text: 'remove',
+      contents: '<i class="fa fa-times fa-lg"></i>',
       class: "remove button small radius alert",
       handler: function() {
         dl.remove();
         app.refreshRoster();
+        app.saveList();
       }
     });
 
-    // create promote button
-    var promoteLink = this.buildLink({
-      text: 'promote',
-      class: "promote button small radius",
+    // create favorite button
+    var favoriteLink = this.buildLink({
+      contents: '<i class="fa fa-star fa-lg"></i>',
+      class: "favorite button small radius",
       handler: function() {
         // just switching the item border for now
         if (dl.style.border === '0px dashed blue')
@@ -82,18 +86,19 @@ var app = {
 
     // create top button
     var topLink = this.buildLink({
-      text: 'top',
+      contents: '<i class="fa fa-arrow-circle-up fa-lg"></i>',
       class: "top button small radius",
       handler: function() {
         // move item to the top
         app.list.insertBefore(dl, dl.parentNode.firstElementChild);
         app.refreshRoster();
+        app.saveList();
       }
     });
 
     // create up button
     var upLink = this.buildLink({
-      text: 'up',
+      contents: '<i class="fa fa-arrow-up fa-lg"></i>',
       class: "up button small radius",
       handler: function() {
         // move item up one space
@@ -101,13 +106,14 @@ var app = {
         if (prevDL !== null) {
           app.list.insertBefore(dl, prevDL);
           app.refreshRoster();
+          app.saveList();
         }
       }
     });
 
     // create down button
     var downLink = this.buildLink({
-      text: 'down',
+      contents: '<i class="fa fa-arrow-down fa-lg"></i>',
       class: "down button small radius",
       handler: function() {
         // move item down one space
@@ -115,6 +121,7 @@ var app = {
         if (nextDL !== null) {
           app.list.insertBefore(dl, nextDL.nextElementSibling);
           app.refreshRoster();
+          app.saveList();
         }
       }
     });
@@ -122,7 +129,7 @@ var app = {
     // put it all together
     ul.appendChild(editLink);
     ul.appendChild(deleteLink);
-    ul.appendChild(promoteLink);
+    ul.appendChild(favoriteLink);
     ul.appendChild(topLink);
     ul.appendChild(upLink);
     ul.appendChild(downLink);
@@ -135,7 +142,7 @@ var app = {
   buildLink: function(options) {
     var link = document.createElement('a');
     link.href = "#";
-    link.innerText = options.text;
+    link.innerHTML = options.contents;
     link.className = options.class;
     link.onclick = options.handler;
 
@@ -170,12 +177,19 @@ var app = {
 
     this.prependChild(this.list, this.buildList(studentName));
     this.refreshRoster();
+    this.roster.unshift({
+      name: studentName,
+    });
     form.reset();
     form.studentName.focus();
   },
 
   prependChild: function(parent, child) {
     parent.insertBefore(child, parent.firstElementChild);
+  },
+
+  saveList: function() {
+    localStorage.setItem('roster', this.list.innerHTML);
   },
 };
 
