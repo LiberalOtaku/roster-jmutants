a27907a80d460e50000b3389c2b7260254ca42be
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-04 22:18:21 -0400

    Added simple implementation of localStorage for data persistency.

--- js/app.js
+++ js/app.js
@@ -4,9 +4,18 @@ var app = {
   // initialize app
   init: function(listSelector) {
     this.list = document.querySelector(listSelector);
-    this.roster = [];
+    this.getLocalStorage();
     this.setupEventListeners();
-    this.list.innerHTML = localStorage.getItem('roster');
+    this.refreshRoster();
+  },
+
+  getLocalStorage: function() {
+    var roster = JSON.parse(localStorage.getItem('roster'));
+    if (roster !== null) {
+      for (var i = 0; i < roster.length; ++i) {
+        this.list.appendChild(this.buildList(roster[i]));
+      }
+    }
   },
 
   setupEventListeners: function() {
@@ -37,7 +46,7 @@ var app = {
     // create edit button
     var editLink = this.buildLink({
       contents: '<i class="fa fa-pencil fa-lg"></i>',
-      class: "edit button small radius secondary",
+      class: "edit button tiny radius secondary",
       handler: function() {
         dt.innerText = '';
         dt.appendChild(input);
@@ -49,7 +58,7 @@ var app = {
     // create update button
     var updateLink = this.buildLink({
       contents: '<i class="fa fa-check fa-lg"></i>',
-      class: "update button small radius success",
+      class: "update button tiny radius success",
       handler: function() {
         if (input.value !== '') {
           dt.innerHTML = '';
@@ -64,7 +73,7 @@ var app = {
     // create delete button
     var deleteLink = this.buildLink({
       contents: '<i class="fa fa-times fa-lg"></i>',
-      class: "remove button small radius alert",
+      class: "remove button tiny radius alert",
       handler: function() {
         dl.remove();
         app.refreshRoster();
@@ -75,7 +84,7 @@ var app = {
     // create favorite button
     var favoriteLink = this.buildLink({
       contents: '<i class="fa fa-star fa-lg"></i>',
-      class: "favorite button small radius",
+      class: "favorite button tiny radius",
       handler: function() {
         // just switching the item border for now
         if (dl.style.border === '0px dashed blue')
@@ -87,7 +96,7 @@ var app = {
     // create top button
     var topLink = this.buildLink({
       contents: '<i class="fa fa-arrow-circle-up fa-lg"></i>',
-      class: "top button small radius",
+      class: "top button tiny radius",
       handler: function() {
         // move item to the top
         app.list.insertBefore(dl, dl.parentNode.firstElementChild);
@@ -99,7 +108,7 @@ var app = {
     // create up button
     var upLink = this.buildLink({
       contents: '<i class="fa fa-arrow-up fa-lg"></i>',
-      class: "up button small radius",
+      class: "up button tiny radius",
       handler: function() {
         // move item up one space
         var prevDL = dl.previousElementSibling;
@@ -114,7 +123,7 @@ var app = {
     // create down button
     var downLink = this.buildLink({
       contents: '<i class="fa fa-arrow-down fa-lg"></i>',
-      class: "down button small radius",
+      class: "down button tiny radius",
       handler: function() {
         // move item down one space
         var nextDL = dl.nextElementSibling;
@@ -150,23 +159,25 @@ var app = {
   },
 
   refreshRoster: function() {
-    var allTop = document.querySelectorAll('a.top');
-    var allUp = document.querySelectorAll('a.up');
-    var allDown = document.querySelectorAll('a.down');
+    if (app.list.firstElementChild !== null) {
+      var allTop = document.querySelectorAll('a.top');
+      var allUp = document.querySelectorAll('a.up');
+      var allDown = document.querySelectorAll('a.down');
 
-    var disableTop = document.querySelector('dl:first-child a.top');
-    var disableUp = document.querySelector('dl:first-child a.up');
-    var disableDown = document.querySelector('dl:last-child a.down');
+      var disableTop = document.querySelector('dl:first-child a.top');
+      var disableUp = document.querySelector('dl:first-child a.up');
+      var disableDown = document.querySelector('dl:last-child a.down');
 
-    for (var i = 0; i < allTop.length; ++i) {
-      allTop[i].className = "top button small radius";
-      allUp[i].className = "up button small radius";
-      allDown[i].className = "down button small radius";
+      for (var i = 0; i < allTop.length; ++i) {
+        allTop[i].className = "top button tiny radius";
+        allUp[i].className = "up button tiny radius";
+        allDown[i].className = "down button tiny radius";
+      }
+
+      disableTop.className += " disabled";
+      disableUp.className += " disabled";
+      disableDown.className += " disabled";
     }
-
-    disableTop.className += " disabled";
-    disableUp.className += " disabled";
-    disableDown.className += " disabled";
   },
 
   // called on form submit
@@ -177,9 +188,7 @@ var app = {
 
     this.prependChild(this.list, this.buildList(studentName));
     this.refreshRoster();
-    this.roster.unshift({
-      name: studentName,
-    });
+    app.saveList();
     form.reset();
     form.studentName.focus();
   },
@@ -189,7 +198,14 @@ var app = {
   },
 
   saveList: function() {
-    localStorage.setItem('roster', this.list.innerHTML);
+    // simplest solution for me to save the list
+    // may be changed to reduce time complexity later
+    var newList = [];
+    var dls = app.list.children;
+    for (var i = 0; i < dls.length; ++i) {
+      newList.push(dls[i].firstElementChild.innerText);
+    }
+    localStorage.setItem('roster', JSON.stringify(newList));
   },
 };
 
